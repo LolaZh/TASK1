194fd69fc92c99347c5b8f33e5a43a83e5094ae3
LolaZh <lolitazhvania@yahoo.com>
2024-11-19 18:38:35 +0400

    Lelas davalebaN3

---
+++ Length.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class Length {
+
+    public static void main(String[] args) {
+
+        String text = "Life is Beautiful!";
+        int textLength = text.length();
+
+        if (textLength % 2 == 0) {
+            System.out.println("სტრიქონის სიგრძე ლუწი რიცხვია!");
+        }
+        else {
+             System.out.println("სტრიქონის სიგრძე კენტი რიცხვია!");
+            }
+
+
+        }
+    }
+
+
