6d450aa2734d33aab06fde6ea6835c9f1553d333
LolaZh <lolitazhvania@yahoo.com>
2024-11-20 17:24:07 +0400

    Lelas davalebaN5

--- Main1.java
+++ Main1.java
@@ -2,19 +2,19 @@ package org.example;
 
 public class Main1 {
 
+        public static void main(String[] args) {
+            hasTeen(1,20,12);
+        }
 
-    public static void main(String[] args) {
-        hasTeen(1,20,12);
+        static void hasTeen(int age1, int age2, int age3) {
+            if ((age1 >= 13 && age1 <= 19)||(age2 >= 13 && age2 <= 19)||(age3 >= 13 && age3 <= 19))
+            {
+                System.out.println("True");
+            }
+            else {
+                System.out.println("False");
+
+            }
+        }
     }
 
-    static void hasTeen(int age1, int age2, int age3) {
-        if ((age1 >= 13 && age1 <= 19)||(age2 >= 13 && age2 <= 19)||(age3 >= 13 && age3 <= 19))
-        {
-            System.out.println("True");
-        }
-        else {
-            System.out.println("False");
-
-        }
-    }
-}
